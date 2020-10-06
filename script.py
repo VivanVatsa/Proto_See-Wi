19f98be109c45f010ea0df3158d319333f2183d8
Vivan <vivanvatsa@gmail.com>
2020-10-06 22:04:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ import cv2
 
 # 0 = black image
 # -1 = color image
-# img = cv2.imread("galaxy.jpg", 0)
-img = cv2.imread("galaxy.jpg", 1)
+img = cv2.imread("galaxy.jpg", 0)
+# img = cv2.imread("galaxy.jpg", 1)
 
 print(type(img))
 print(img)
@@ -19,5 +19,6 @@ resized_image = cv2.resize(img,(int(img.shape[1]/2), int(img.shape[0]/2)))
 # cv2.imshow("Galaxy", img)
 cv2.imshow("Galaxy", resized_image)
 # cv2.waitKey(2000) #this acts in milliseconds
+cv2.imwrite("Galaxy_resized.jpg", resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
