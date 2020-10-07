85efbdbc2cab7931a095111b1697f1c13a5a03ad
Vivan <vivanvatsa@gmail.com>
2020-10-07 10:16:26 +0530

    Update resize_script.py

--- resize_assets/resize_script.py
+++ resize_assets/resize_script.py
@@ -5,8 +5,8 @@ images=glob.glob("*.jpg")
 
 for image in images:
     img=cv2.imread(image, 0)
-    re=cv2.resize(img, (100,100))
+    re=cv2.resize(img, (300,300))
     cv2.imshow("Hey", re)
     cv2.waitKey(500)
     cv2.destroyAllWindows()
-    cv2.imwrite("resized"+image, res)
+    cv2.imwrite("resized_"+image, re)
