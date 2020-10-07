8c4292af57621c5b8dbc6b4c00bc464c845834f8
Vivan <vivanvatsa@gmail.com>
2020-10-07 21:30:54 +0530

    Update face_detector.py

--- face_detect_algo/face_detector.py
+++ face_detect_algo/face_detector.py
@@ -4,8 +4,8 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 # img=cv2.imread("photo.jpg", 0)
 img=cv2.imread("photo.jpg")
 
-# gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 faces=face_cascade.detectMultiScale(gray_img, 
                                     scaleFactor=1.05,
